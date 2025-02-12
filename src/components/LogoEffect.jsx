fb74c64da0459b5e31bfc0654dfe0f5c3d1dbe6e
haitham <haitham9559@gmail.com>
2025-02-12 04:13:51 +0300

    home in progress 5

--- src/components/LogoEffect.jsx
+++ src/components/LogoEffect.jsx
@@ -17,7 +17,7 @@ function LogoEffect({ normalizedY, scrollYProgress, topValue }) {
           <div className="header-logo-backdrop-white-2" style={{}}></div> */}
         </div>
         <div className="header-logo-img-container">
-          <img src="/images/mega-logo-blue.png" alt="Mega Group Logo" />
+          <img src="/images/mega-logo-notext.png" alt="Mega Group Logo" />
           {/* <img
             src="/images/mega-logo-header-loading.png"
             alt="Mega Group Logo"
